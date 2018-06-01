ccbd1a5c2030aa84fb03a98a273d8ccf7971a1bd
Rishabh Poddar <rishabhp89@gmail.com>
2018-06-01 12:01:06 -0700

    undo previous commit

--- vim/setup.sh
+++ vim/setup.sh
@@ -4,14 +4,3 @@ cd ~
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-# download solarized color scheme for vim
-wget http://ethanschoonover.com/solarized/files/solarized.zip
-unzip solarized.zip
-mkdir .vim
-mkdir .vim/colors/
-mv solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
-curl https://raw.github.com/seebi/dircolors-solarized/master/dircolors.256dark > ~/.dircolors
-source .bashrc
-rm -r solarized
-rm solarized.zip
